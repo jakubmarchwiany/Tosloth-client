d9123f8030875bd4b7d9107dd79e0bbdb0ad3e25
xkvbikx <50952542+xkvbikx@users.noreply.github.com>
2021-02-24 16:05:36 +0100

    0.0.1 Alpha

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,35 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        try {
+
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            Parent splashView = fxmlLoader.load((getClass().getResource("splashView.fxml").openStream()));
+            Scene scene = new Scene(splashView);
+            primaryStage.setScene(scene);
+            primaryStage.initStyle(StageStyle.TRANSPARENT);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
